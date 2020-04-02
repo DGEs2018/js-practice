70ec59f14baa3341125a153bd7d10d73d6f0b4d2
Dawit <dawit@ginetta.net>
2020-04-02 15:25:23 +0200

    Abbreviate the lines of code returning false in one line of code

--- app.js
+++ app.js
@@ -13,19 +13,26 @@
 // isValidPassword('cdmHonk', 'cdmHonk'); false
 //isValidPassword('hello1', 'cdmHonk'); false
 
+// const isValidPassword = (password, username) => {
+// 	if (password.length < 8) {
+// 		// problem with password.length >= 8, is that if it's true, then the if statement won't continue
+// 		return false;
+// 	}
+// 	if (password.indexOf(' ') !== -1) {
+// 		// if password doesn't contain spaces
+// 		return false;
+// 	}
+// 	if (password.indexOf(username)) {
+// 		// if password doesn't contain the username
+// 		return false;
+// 	} else
+// 		// then, as all the requirement are fulfilled, then return true
+// 		return true;
+// };
+
 const isValidPassword = (password, username) => {
-	if (password.length < 8) {
-		// problem with password.length >= 8, is that if it's true, then the if statement won't continue
+	if (password.length < 8 || password.indexOf(' ') !== -1 || password.indexOf(username) !== -1) {
 		return false;
 	}
-	if (password.indexOf(' ') !== -1) {
-		// if password doesn't contain spaces
-		return false;
-	}
-	if (password.indexOf(username)) {
-		// if password doesn't contain the username
-		return false;
-	} else
-		// then, as all the requirement are fulfilled, then return true
-		return true;
+	return true;
 };
