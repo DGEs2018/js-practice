d4dcdd1346c9e2c74f64e89a1ce4d6b7d6edf51b
Dawit <dawit@ginetta.net>
2020-04-02 12:25:31 +0200

    Add create an app.js file with lines of pseudo-codes

---
+++ app.js
@@ -0,0 +1,14 @@
+// Write an isValidPassword function
+
+// that accepts 2 arguments: password and username
+
+// password must:
+// - be at least 8 characters long
+// - cannot contain spaces
+// - cannot contain the username
+// - if all requirements are met, return true.
+// otherwise: false
+
+// isValidPassword('89Fjj1nms', 'cdmHonk'); true
+// isValidPassword('cdmHonk', 'cdmHonk'); false
+//isValidPassword('hello1', 'cdmHonk'); false
